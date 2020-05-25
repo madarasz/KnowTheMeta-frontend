0a3d988e685c9f4030ab55dfff2a141deb485385
madarasz <mr.madarasz.istvan@gmail.com>
2020-05-25 12:11:59 +0200

    mwl badge refactor

--- src/components/widget/NetrunnerCard.vue
+++ src/components/widget/NetrunnerCard.vue
@@ -4,9 +4,11 @@
       <v-col class="text-center pa-1 pb-0">
         <div class="d-inline-flex">
           <v-img :src="card.image_url" width="100px" height="140px">
-            <v-badge bordered overlap offset-x="-10" content="new" v-if="card.badge === 'new'" color="green" label="new in MWL"/>
-            <v-badge class="badge-border-fix" bordered overlap color="blue" :content="card.badge" :label="card.badge + 'in the previous MWL'"
-                v-if="card.badge && card.badge.indexOf('was') > -1" :offset-x="26.25 + card.badge.length * 1.25" offset-y="30"/>
+            <v-badge v-if="card.badge" class="badge-border-fix" bordered overlap
+                :content="card.badge" :label="card.badge"
+                :color="card.badge === 'new' ? 'green': 'blue'"
+                :offset-x="card.badge === 'new' ? -10 : 26.25 + card.badge.length * 1.25"
+                :offset-y="card.badge === 'new' ? 0 : 30"/>
           </v-img>
         </div>
       </v-col>
