cb5d43d80206144aa4586b293287da474eddf232
madarasz <mr.madarasz.istvan@gmail.com>
2020-05-30 22:02:40 +0200

    remove draw from side winrate chart if insignificant

--- src/components/chart/SideWinrateChart.vue
+++ src/components/chart/SideWinrateChart.vue
@@ -61,34 +61,39 @@ export default {
   },
   computed: {
     chartdata: function () {
+      const datasets = [
+        {
+          label: 'runner',
+          data: [Math.round(this.runnerWin * 1000) / 10],
+          backgroundColor: ['#FF0000'],
+          barThickness: 32,
+          barPercentage: 1,
+          categoryPercentage: 1
+        },
+        {
+          label: 'draw',
+          data: [Math.round(this.draw * 1000) / 10],
+          backgroundColor: ['#AAAAAA'],
+          barThickness: 32,
+          barPercentage: 1,
+          categoryPercentage: 1
+        },
+        {
+          label: 'corporation',
+          data: [Math.round(this.corpWin * 1000) / 10],
+          backgroundColor: ['#0000FF'],
+          barThickness: 32,
+          barPercentage: 1,
+          categoryPercentage: 1
+        }
+      ]
+      // remove draw part if draw < 0.005
+      if (this.draw < 0.005) {
+        datasets.splice(1, 1)
+      }
       return {
         labels: ['side winrate'],
-        datasets: [
-          {
-            label: 'runner',
-            data: [Math.round(this.runnerWin * 1000) / 10],
-            backgroundColor: ['#FF0000'],
-            barThickness: 32,
-            barPercentage: 1,
-            categoryPercentage: 1
-          },
-          {
-            label: 'draw',
-            data: [Math.round(this.draw * 1000) / 10],
-            backgroundColor: ['#AAAAAA'],
-            barThickness: 32,
-            barPercentage: 1,
-            categoryPercentage: 1
-          },
-          {
-            label: 'corporation',
-            data: [Math.round(this.corpWin * 1000) / 10],
-            backgroundColor: ['#0000FF'],
-            barThickness: 32,
-            barPercentage: 1,
-            categoryPercentage: 1
-          }
-        ]
+        datasets: datasets
       }
     }
   }
