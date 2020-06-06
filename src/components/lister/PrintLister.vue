1998e30a744629b0db9f8ccbfbc5f803aefb008c
madarasz <mr.madarasz.istvan@gmail.com>
2020-06-06 15:20:13 +0200

    print lister style fix

--- src/components/lister/PrintLister.vue
+++ src/components/lister/PrintLister.vue
@@ -2,7 +2,7 @@
   <v-carousel v-if="prints" :show-arrows="prints.length > 1" hide-delimiters style="margin-bottom: -32px" :continuous="false">
     <v-carousel-item v-for="print in prints" :key="print.code" class="text-center">
       <v-row dense>
-        <v-col class="text-center">
+        <v-col class="text-center pb-0">
           <div class="d-inline-flex">
           <v-img :src="print.image_url" width="300px" height="420px"
                 :lazy-src="'/img/' + (isRunner ? 'runner' : 'corp') + '-card-back-small.png'" />
@@ -10,7 +10,7 @@
         </v-col>
       </v-row>
       <v-row dense>
-        <v-col>
+        <v-col class="pt-0">
           <em>{{ print.pack_title }}</em>
         </v-col>
       </v-row>
