4bc3b42c0bc2522d481ff0ac30ffb1b074c86a6a
madarasz <mr.madarasz.istvan@gmail.com>
2020-06-10 20:24:30 +0200

    card search bugfix

--- src/components/widget/CardAutocomplete.vue
+++ src/components/widget/CardAutocomplete.vue
@@ -2,9 +2,9 @@
   <div>
     <!-- desktop -->
     <v-autocomplete :append-icon="mdiMagnify" :allow-overflow="false" clearable persistent-hint
-      :items="cardTitles" :hide-no-data="hideFiltered" :search-input.sync="searchString" :loading="loading"
+      :items="cardTitles" :hide-no-data="hideNoData" :search-input.sync="searchString" :loading="loading"
       placeholder="search for card" no-data-text="no such card" class="mr-4 mt-4"
-      v-model="selected" @change="cardSelected" v-if="$vuetify.breakpoint.mdAndUp">
+      v-model="selected" @change="cardSelected" v-if="$vuetify.breakpoint.mdAndUp" data-testid="card-search">
     </v-autocomplete>
     <!-- mobile -->
     <div v-if="!$vuetify.breakpoint.mdAndUp">
@@ -12,9 +12,9 @@
         <v-icon>{{ mdiMagnify }}</v-icon>
       </v-btn>
       <v-autocomplete :append-icon="mdiMagnify" :allow-overflow="false" clearable persistent-hint
-        :items="cardTitles" :hide-no-data="hideFiltered" :search-input.sync="searchString" :loading="loading"
+        :items="cardTitles" :hide-no-data="hideNoData" :search-input.sync="searchString" :loading="loading"
         placeholder="card search" no-data-text="no such card" class="ml-2 mt-4"
-        v-model="selected" @change="cardSelected" v-show="mobileSearch"
+        v-model="selected" @change="cardSelected" v-show="mobileSearch" data-testid="card-search"
         ref="mobileautocomplete" @blur="searchMobile(false)">
       </v-autocomplete>
     </div>
@@ -40,32 +40,25 @@ export default {
     loading: false, // loading animation
     selected: {}, // selected item
     searchString: '', // search string
-    hideFiltered: true // wether to hide "no such card message" because search character count is not enough
+    hideNoData: true // wether to hide "no such card message" because search character count is not enough
   }),
   mounted: function () {
   },
   methods: {
     cardSelected: function (title) {
       const code = Object.keys(this.netrunnerdb.prints).find(x => this.netrunnerdb.prints[x] === title)
-      const url = transform.cardUrl({ code, title })
-      this.selected = {}
-      // fire analytics
-      this.$ga.event({
-        eventCategory: 'Search',
-        eventAction: 'card-search',
-        eventLabel: title
-      })
-      // navigate to card stats
-      this.$router.push({ path: url })
-    },
-    filterFunction: function (item, queryText, itemText) {
-      // hide no card found message if seach string is shorten than 2 chars
-      if (queryText.length < this.minChars) {
-        this.hideFiltered = true
-        return false
+      if (code) {
+        const url = transform.cardUrl({ code, title })
+        this.selected = {}
+        // fire analytics
+        this.$ga.event({
+          eventCategory: 'Search',
+          eventAction: 'card-search',
+          eventLabel: title
+        })
+        // navigate to card stats
+        this.$router.push({ path: url })
       }
-      this.hideFiltered = false
-      return itemText.toLocaleLowerCase().indexOf(queryText.toLocaleLowerCase()) > -1
     },
     searchMobile: function (enable) {
       this.$emit('mobile-search', enable)
@@ -80,8 +73,10 @@ export default {
   },
   watch: {
     searchString: function (newValue, oldValue) {
+      // hide no data message if not enough characters
+      this.hideNoData = this.loading || !newValue || newValue.length < this.minChars
       // load NetrunnerDB if cards are missing
-      if (newValue && newValue.length >= this.minChars && Object.keys(this.netrunnerdb.cards).length === 0) {
+      if (newValue && newValue.length >= this.minChars && Object.keys(this.netrunnerdb.cards).length === 0 && this.loading === false) {
         this.loading = '#638098'
         this.$store.dispatch('netrunnerdb/getCardData', false).then(() => {
           this.loading = false
