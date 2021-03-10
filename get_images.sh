d55d5af8fc0934a531e48263fd9199db5cc460bf
madarasz <mr.madarasz.istvan@gmail.com>
2021-03-10 14:28:04 +0100

    get images. store them locally

---
+++ get_images.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# you need "jq" installed to run this script
+
+# get card list
+curl https://netrunnerdb.com/api/2.0/public/cards > cards.json
+
+# get card data
+codes=( $(cat cards.json | jq -r '.data[].code' ) )
+imageurls=( $(cat cards.json | jq -r '.data[].image_url' ) )
+titles=( $(cat cards.json | jq -r '.data[].title' | tr -s ' ' | tr ' ' '-' | tr '[:upper:]' '[:lower:]' | sed "s/[^a-z0-9.-]//g") )
+i=0
+
+# iterate over cards
+for card in "${codes[@]}"
+do
+    if [ ! -f "public/img/cards/$card.png" ]; then
+        imageurl=${imageurls[$i]}
+        title=${titles[$i]}
+        echo "Downloading card image: $title"
+
+        if [ "$imageurl" == "null" ]; then
+            # download from NetrunnerDB
+            curl "https://netrunnerdb.com/card_image/$card.png" > "public/img/cards/$card.png"
+        else
+            # download from CardGameDB
+            curl $imageurl > "public/img/cards/$card.png"
+        fi
+    fi
+    ((i++))
+done
